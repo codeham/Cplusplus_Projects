1ab4e1938bffbdc9131cac8915f41644dfa5150d
Cristian Avina <shaolinvswutang94@gmail.com>
2016-03-23 10:31:39 -0700

    Delete Hello.cpp

--- Hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Goodbye, World!" << std::endl;
-    std::cout << sizeof (long double) <<std::endl;
-    return 0;
-}
